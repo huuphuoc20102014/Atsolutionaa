f7f3567c384fc9ba276fb3276bb63be1b501c73e
Truong <truongpq197@gmail.com>
2019-09-17 14:50:47 +0700

    logo

---
+++ Atsolution/Helpers/MenuHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Atsolution.Efs.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atsolution.Helpers
+{
+    public class MenuHelper
+    {
+        public static async Task<List<Menu>> GetDataMenu(WebATSolutionContext webContext)
+        {
+            return await webContext.Menu.ToListAsync();
+        }
+    }
+}
